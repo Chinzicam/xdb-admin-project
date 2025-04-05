5b4c6c177459434c34efd0bcb3a8ddb71eb03713
Chinzicam <a3189797316@163.com>
2025-04-06 01:02:05 +0800

    fix

--- x-admin-web/src/api/userManage.js
+++ x-admin-web/src/api/userManage.js
@@ -1,49 +1,49 @@
 import request from '@/utils/request'
 
-export default{
-  getUserList(searchModel){
+export default {
+  getUserList(searchModel) {
     return request({
       url: '/user/list',
       method: 'get',
-      params:{
+      params: {
         pageNo: searchModel.pageNo,
         pageSize: searchModel.pageSize,
         username: searchModel.username,
         phone: searchModel.phone
       }
-    });
+    })
   },
-  addUser(user){
+  addUser(user) {
     return request({
       url: '/user',
       method: 'post',
       data: user
-    });
+    })
   },
-  updateUser(user){
+  updateUser(user) {
     return request({
       url: '/user',
       method: 'put',
       data: user
-    });
+    })
   },
-  saveUser(user){
-    if(user.id == null && user.id == undefined){
-      return this.addUser(user);
+  saveUser(user) {
+    if (user.id == null && user.id == undefined) {
+      return this.addUser(user)
     }
-    return this.updateUser(user);
+    return this.updateUser(user)
   },
-  getUserById(id){
+  getUserById(id) {
     return request({
-      //url: '/user/' + id,
+      // url: '/user/' + id,
       url: `/user/${id}`,
       method: 'get'
-    });
+    })
   },
-  deleteUserById(id){
+  deleteUserById(id) {
     return request({
       url: `/user/${id}`,
       method: 'delete'
-    });
-  },
+    })
+  }
 }
